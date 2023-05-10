e8ce15887be30b782aad73b5ba8c04940679a103
Abhi Patel <abhi.d@simformsolutions.com>
2023-05-10 14:44:34 +0530

    update : Program.cs file where given task perform inside a main method.

--- CSharp_PR_5/Program.cs
+++ CSharp_PR_5/Program.cs
@@ -4,7 +4,23 @@
 	{
 		static void Main(string[] args)
 		{
-			
+			int[] numbers = { 1, 2, 3, 4, 5 };
+
+			for (int i = 0; i <= 5; i++)
+			{
+				try
+				{
+					Console.WriteLine($"\nindex : {numbers[i]}");
+				}
+				catch (IndexOutOfRangeException e)
+				{
+					Console.WriteLine($"\nException message is : {e.Message}");
+				}
+				finally
+				{
+					Console.WriteLine("\nThis is finally block");
+				}
+			}
 		}
 	}
 }
